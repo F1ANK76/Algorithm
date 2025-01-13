b9acaebabe3220857cdf270d5dab7e61f60b96d9
F1ANK76 <ygodlksg@gmail.com>
2025-01-13 09:38:05 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.74 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.cpp
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.cpp
@@ -4,14 +4,21 @@
 using namespace std;
 
 int solution(string t, string p) {
-    int answer = 0;
-    int pLen = p.length();
+    int count = 0;
+    long long pValue = stoll(p);  // p를 64비트 정수로 변환
     
-    for (int i = 0; i <= t.length() - pLen; i++) {
-        string tmp = t.substr(i, pLen);
-        long tmpInt = stol(tmp), pInt = stol(p);
+    int tLength = t.size();
+    int pLength = p.size();
+    
+    // t에서 p와 같은 길이의 부분문자열을 하나씩 확인
+    for (int i = 0; i <= tLength - pLength; i++) {
+        string sub = t.substr(i, pLength);  // t에서 부분문자열 추출
+        long long subValue = stoll(sub);    // 부분문자열을 64비트 정수로 변환
         
-        if (tmpInt <= pInt) answer += 1;
+        if (subValue <= pValue) {
+            count++;  // 조건을 만족하면 카운트
+        }
     }
-    return answer;
+    
+    return count;
 }
